d99c59c38aa9517a81010d98078dcd142f855617
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-03-19 09:51:07 -0300

    lists

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>PLP - UFCG</h1>
+<p>Olá , criei este repositorio para armazenar o conteúdo visto na disciplina <strong>Paradigmas de Linguagem de Programação (PLP)</strong> na UFCG.</p>
+<hr />
---
+++ learningHaskhel/listas.hs
@@ -0,0 +1,11 @@
+let oddNumbers = [1,3,5,7,9];
+let evenNumbers = [2,4,6,8,10];
+
+let naturals = oddNumbers ++ evenNumbers;
+
+{-
+ghci> "hello" ++ " " ++ "world"  
+    "hello world"  
+ghci> ['w','o'] ++ ['o','t']  
+    "woot"  
+-}
