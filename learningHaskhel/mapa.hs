8630a7f294697110b0f2a804c1f92b9797b80a24
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-03-30 21:05:10 -0300

    Map

---
+++ learningHaskhel/mapa.hs
@@ -0,0 +1,33 @@
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
+
+
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+{- Testes de filter'
+ filter' (>3) [1,2,3,4,5,6,7,8,9,10]
+ filter' (==3) [1,2,3,4,5,6,7,8,9,10]
+ filter' (<3) [1,2,3,4,5,6,7,8,9,10]
+ filter' (<=3) [1,2,3,4,5,6,7,8,9,10]
+-}
+
+
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) =
+    let smallerSorted = quickSort(filter' (<=x) xs)
+        biggerSorted  = quickSort(filter' (>x) xs)
+    in smallerSorted ++ [x] ++ biggerSorted
+
+{- Testes de quickSort
+    quickSort [1,2,3,4,5]
+    quickSort [2,5,8,3,6,2,9,10]
+-}
+
